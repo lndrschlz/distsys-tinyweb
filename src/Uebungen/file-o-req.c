5efdc29827a8d12f199d5675c54571bd1782bd54
Jonas Polkehn <jonas.polkehn@googlemail.com>
2015-05-23 16:13:58 +0200

    Uebung zwei

---
+++ src/Uebungen/file-o-req.c
@@ -0,0 +1,73 @@
+/***
+* Programm: file-o-req
+* Laboruebung 1, Aufgabe 2
+* Schulz, Reutebuch, Polkehn (TIT12)
+***/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "connect_tcp.h"
+
+
+#define BUFSIZE 1000
+
+int main(int argc, char **argv)
+{
+	// Check if all required arguments are given
+	if (argc < 4 )
+	{
+		fprintf(stderr, "Error: Only %d of 3 required arguments given\n", argc-1);
+		exit(1);
+	}
+	
+	// Open a tcp socket to the given server
+	int port = atoi(argv[2]);
+	int sd = connect_tcp(argv[1], port);
+	
+	// Open the given text file
+	int fd = open(argv[3], O_RDONLY);
+	
+	// Loop and read the text file and write the buffer to the socket
+	int cc;
+	char buf[BUFSIZE];
+	
+	while ((cc = read(fd, buf, BUFSIZE)))
+	{
+		if (cc < 0)
+		{
+			fprintf(stderr, "Error: %d when reading file!", cc);
+			exit(1);
+		}
+		
+		write(sd, buf, cc);
+	}
+	
+	close(fd);
+	
+	// Terminate the request
+	write(sd, '\0', 1);
+		
+	// Loop and read the server response, write response to stdout
+	while ((cc = read(sd, buf, BUFSIZE)))
+	{
+		if (cc < 0)
+		{
+			fprintf(stderr, "Error: %d when reading server response!", cc);
+			exit(1);
+		}
+		
+		fwrite(buf, sizeof(char), cc, stdout);
+	}
+		
+	// Close the socket
+	close(sd);
+	
+	// Exit the programmn
+	exit(0);
+	
+}
