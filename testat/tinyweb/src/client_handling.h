c49200e75511b6afcc733543664033aefde67546
Lenny <leanderschulz@gmx.de>
2015-06-11 10:44:10 +0000

    Module Header client_handle.h

--- testat/tinyweb/src/client_handling.h
+++ testat/tinyweb/src/client_handling.h
@@ -1,3 +1,10 @@
+/*
+ * module name:         tinyweb.h
+ * author:              Jonas Pohlken, Leander Schulz, Christian Reutebuch
+ * purpose:             headerfile for client_handle module
+ *
+ */
+
 int accept_client(int sd, int nsd);
 int send_response(http_res_t * response,int sd);
 int parse_request(http_req_t * request, char *req_string);
