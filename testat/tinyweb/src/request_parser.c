8cd9f6352760c2a2d936a09b7e44dd1bbf4fd9d1
Jonas Polkehn <devjp@posteo.de>
2015-06-11 13:14:14 +0200

    parse method complete and compiles

--- testat/tinyweb/src/request_parser.c
+++ testat/tinyweb/src/request_parser.c
@@ -1,6 +1,47 @@
+
+#include <string.h>
 #include <tinyweb.h>
+
+#define BUFSIZE 100000
+
+
+int parse_resource_string(char * buffer, http_req_t * request)
+{
+	return 0;	
+}
+
+int parse_method(char * buffer, http_req_t * request){
+	char * walker; 
+	
+	walker = buffer;
+	
+	if (*walker == 'G')
+	{
+		if (*(++walker) != 'E') { return -1;}
+		if (*(++walker) != 'T') { return -1;}
+		request->method = HTTP_METHOD_GET;
+	}
+	if (*walker == 'H')
+	{
+		if (*(++walker) != 'E') { return -1;}
+		if (*(++walker) != 'A') { return -1;}
+		if (*(++walker) != 'D') { return -1;}
+		request->method = HTTP_METHOD_HEAD;
+	}
+	
+	if (*(++walker) != ' ') { return -1;}
+	
+	int err = parse_resource_string(walker, request);
+	return err;
+}
 
 int parse_request(http_req_t * request, char *req_string)
 {
-	return 0;
+	char buffer[BUFSIZE];
+		
+	strcpy(buffer, req_string);
+	
+	int err = parse_method(buffer, request);
+	return err;
 }
+
